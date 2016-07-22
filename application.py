37722bbc9119fd72fce06fa043a0f314278db44f
charlieHall <ch12953@my.bristol.ac.uk>
2016-07-22 11:46:14 +0100

    changes to .py

--- application.py
+++ application.py
@@ -2,35 +2,35 @@ from flask import Flask, render_template, request
 from flask.ext.mobility import Mobility
 from flask.ext.mobility.decorators import mobile_template
 
-app = Flask(__name__)
-app.debug=True
+application = Flask(__name__)
+application.debug=True
 
-Mobility(app)
+Mobility(application)
 
-@app.route('/')
-@app.route('/index')
+@application.route('/')
+@application.route('/index')
 @mobile_template('{mobile/}index')
 def index(template):
     return render_template(template + ".html")
 
 
-@app.route('/aboutUs')
+@application.route('/aboutUs')
 @mobile_template('{mobile/}aboutUs')
 def about(template):
 	return render_template(template + ".html")
 
-@app.route('/TermsAndConditions')
+@application.route('/TermsAndConditions')
 def terms():
 	return render_template('Terms_and_Conditions.html')
 
-@app.route('/socialMedia')
+@application.route('/socialMedia')
 def temp():
     return render_template('socialMedia.html')
 
-@app.route('/template')
+@application.route('/template')
 def social():
     return render_template('template.html')
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.22', port=5002, threaded=True, debug=True)
+    application.run()
 
